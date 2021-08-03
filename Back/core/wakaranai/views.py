73b153bfa8c165ba8218dc5ce0d1739a6447451a
ybykov <ybykov@emphasoft.com>
2021-08-03 22:49:51 +0800

    django-config

--- Back/core/wakaranai/views.py
+++ Back/core/wakaranai/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.conf import settings
+from django.conf.urls.static import static
 
-# Create your views here.
+
+def index_page(request):
+    return render(request, "Front/index.html")
