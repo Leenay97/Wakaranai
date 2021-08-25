05d01c9e72ec6dd3e0b48454deee459e471f8dee
Leenay97 <skynekop@gmail.com>
2021-08-26 02:22:54 +0800

    tried to fix bugs

--- core/settings.py
+++ core/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 
 from dotenv import load_dotenv
 
@@ -148,3 +149,5 @@ STATICFILES_DIRS = (
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+django_heroku.settings(locals())
